1bc49c779f0726376f7280fb9942b4ce857862c4
Andrei Dzebysh <presnia@yahoo.com>
2020-07-14 19:21:52 +0300

    Changed the name of main file to myfizzbuzz.js

---
+++ src/myfizzbuzz.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* for (let i = 1; i <= 100; i = i + 1) {
+  if (i % 3 === 0) {
+    console.log('fizz');
+  }
+  if (i % 5 === 0) {
+    console.log('buzz');
+  }
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('fizzbuzz');
+  }else {
+    console.log(i);
+  }
+  
+} */
+
+for (let i = 1; i < 10; i = i +1) {
+  console.log(i);
+  
+}
