df8143b828d04ea23cf32cb891988f96c7f7bf4f
Andrei Dzebysh <presnia@yahoo.com>
2020-07-14 21:48:29 +0300

    Finished

--- src/myfizzbuzz.js
+++ src/myfizzbuzz.js
@@ -1,20 +1,20 @@
 "use strict";
 
-/* for (let i = 1; i <= 100; i = i + 1) {
-  if (i % 3 === 0) {
-    console.log('fizz');
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i = i + 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    }
+    else if (i % 5 === 0) {
+      console.log("buzz");
+    }
+    else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      let a = i.toString;
+      console.log(i.toString());
+    }
   }
-  if (i % 5 === 0) {
-    console.log('buzz');
-  }
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log('fizzbuzz');
-  }else {
-    console.log(i);
-  }
-  
-} */
-
-for (let i = 1; i < 10; i = i +1) {
-  console.log(i);
 }
+
+fizzBuzz();
